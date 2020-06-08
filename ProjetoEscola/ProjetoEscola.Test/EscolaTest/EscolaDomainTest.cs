b67f4e06c5c993048748755aaf19bf4ad8051bad
Rafael <Teles>
2020-06-08 16:34:27 -0300

    Adicionando testes unitarios em escola

---
+++ ProjetoEscola/ProjetoEscola.Test/EscolaTest/EscolaDomainTest.cs
@@ -0,0 +1,36 @@
+﻿using ProjetoEscola.Dominio.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ProjetoEscola.Test.DominioTeste
+{
+    public class EscolaDomainTest
+    {
+        [Fact(DisplayName = "Adicionar Escola com sucesso")]
+        [Trait("Categoria","Escola Trait Test")]
+        public void Escola_InformarONome_RetornaSucesso()
+        {
+            //Arrange
+            string nomeEscola = "Ciep 001";
+
+            //Act
+            var escola = new Escola(nomeEscola);
+
+            //Assert
+            Assert.NotNull(escola);
+        }
+
+        [Fact(DisplayName = "Escola não informada tem que retornar erro")]
+        [Trait("Categoria", "Escola Trait Test")]
+        public void Escola_NaoInformarONome_RetornaErro()
+        {
+            //Arrange
+            string nomeEscola = "";
+
+            //Assert
+            Assert.Throws<Exception>(() => new Escola(nomeEscola));
+        }
+    }
+}
---
+++ ProjetoEscola/ProjetoEscola.Test/EscolaTest/EscolaServiceTest.cs
@@ -0,0 +1,54 @@
+﻿using Moq;
+using ProjetoEscola.Aplicacao.Aplicacao;
+using ProjetoEscola.Dominio.Entidade;
+using ProjetoEscola.Dominio.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ProjetoEscola.Test.TurmaTest
+{
+    public class EscolaServiceTest
+    {
+        [Fact(DisplayName = "Adicionar Escola com sucesso")]
+        [Trait("Categoria", "Escola Service Test")]
+        public void EscolaService_Adicionar_RetornarSucesso()
+        {
+            //Arrange
+            var escolaRepo = new Mock<IEscolaRepository>();
+            var escolaApplication = new EscolaApplication(escolaRepo.Object);
+
+            var escola = new Escola("CIEP 2200");
+
+            //Act 
+            escolaApplication.Adicionar(escola);
+
+            //Assert
+            escolaRepo.Verify(e => e.Adicionar(escola), Times.Once);
+
+
+        }
+
+
+
+        [Fact(DisplayName = "Atualizar Escola com sucesso")]
+        [Trait("Categoria", "Escola Service Test")]
+        public void EscolaService_Atualizar_RetornarSucesso()
+        {
+            //Arrange
+            var escolaRepo = new Mock<IEscolaRepository>();
+            var escolaApplication = new EscolaApplication(escolaRepo.Object);
+
+            var escola = new Escola("CIEP 2200");
+
+            //Act 
+            escolaApplication.Atualizar(escola);
+
+            //Assert
+            escolaRepo.Verify(e => e.Atualizar(escola), Times.Once);
+
+
+        }
+    }
+}
